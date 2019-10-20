ba4264aa5bbd9f4bf62f96fee05a2b0bb60bc99b
cletsxdv <jclementehdzhdz@gmail.com>
2019-10-20 12:35:25 -0500

    ya casi queda

--- MIU-system.py
+++ MIU-system.py
@@ -51,13 +51,66 @@ def rule4(word) :
           
           return wr4, 1
       
-    return word, 0  
-        
-        
+    return word, 0
 
-word = input('Enter the word: ')
+def search_rules(words):
+    ret = []
 
-wr1, check1 = rule1(word)
-wr2, check2 = rule2(word)
-wr3, check3 = rule3(word)
-wr4, check4 = rule4(word)
+    if len(words) < 2:
+        wr1, check1 = rule1(words[0])
+        wr2, check2 = rule2(words[0])
+        wr3, check3 = rule3(words[0])
+        wr4, check4 = rule4(words[0])
+
+        if check1 == 1:
+            ret.append(wr1)
+
+        if check2 == 1:
+            ret.append(wr2)
+
+        if check3 == 1:
+            ret.append(wr3)
+
+        if check4 == 1:
+            ret.append(wr4)
+    else:
+        for word in words:
+            wr1, check1 = rule1(word)
+            wr2, check2 = rule2(word)
+            wr3, check3 = rule3(word)
+            wr4, check4 = rule4(word)
+
+            if check1 == 1:
+                ret.append(wr1)
+
+            if check2 == 1:
+                ret.append(wr2)
+
+            if check3 == 1:
+                ret.append(wr3)
+
+            if check4 == 1:
+                ret.append(wr4)
+
+    return ret
+
+def check_mu(words):
+    if "MU" in words:
+        return 1
+    else:
+        return 0
+
+if __name__ == '__main__':
+    word = input('Enter the word: ')
+
+    first_attp = []
+    first_attp.append(word)
+
+    list_words = []
+    list_words = search_rules(first_attp)
+
+    print(list_words)
+
+    list_words = search_rules(list_words)
+
+    print(list_words)
